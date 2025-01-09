ba8f354737cc6acb445cf04106ad70d1774ff7ed
BartCatalano <catalanobartolomeo@gmail.com>
2025-01-09 15:06:34 +0100

    feat: pagine create e Router usato. navbar(WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import AppLayout from "./components/AppLayout"
+import HomePage from "./components/HomePage"
+import RecipePage from "./components/RecipePage"
 
 
 function App() {
+return(
+<BrowserRouter>
+<Routes>
+  <Route element={<AppLayout />}>
+<Route path="/"element={<HomePage /> } />
+<Route path="/Recipe"element={<RecipePage/>} />
+  </Route>
+</Routes>
+</BrowserRouter>
 
+)
 
 }
 export default App
