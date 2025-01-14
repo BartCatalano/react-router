3d828b4f4716d33717a8f0c0fc3cfcf01e883952
BartCatalano <catalanobartolomeo@gmail.com>
2025-01-14 16:17:51 +0100

    feat: faccio una pausa

--- src/components/pages/AdminRecipePage.jsx
+++ src/components/pages/AdminRecipePage.jsx
@@ -4,73 +4,74 @@ const apiurl = import.meta.env.VITE_API_URL;
 
 
 
-function AdminRecipePage () {
-   
-
- const [lista, setLista] = useState([]);
+function AdminRecipePage() {
 
 
-useEffect(() => {
-  getData();
-}, [])
-// creo la funzione che richiama axios
-const getData = () => {
-  axios.get(`${apiurl}posts`).then((resp) => {
-    setLista(resp.data.data);
-
-  })
-}
+  const [lista, setLista] = useState([]);
 
 
+  useEffect(() => {
+    getData();
+  }, [])
+  // creo la funzione che richiama axios
+  const getData = () => {
+    axios.get(`${apiurl}posts`).then((resp) => {
+      setLista(resp.data.data);
+
+    })
+  }
 
 
-// creo funzione per cancellare il post inserito
- const cancella = (id) => {
-  axios.delete(`${apiurl}posts/${id}`).then((resp) => {
- const newArray = lista.filter(curArticolo => curArticolo.id !== id);
-
-   setLista(newArray)
- }) }
 
 
-return (
+  // creo funzione per cancellare il post inserito
+  const cancella = (id) => {
+    axios.delete(`${apiurl}posts/${id}`).then((resp) => {
+      const newArray = lista.filter(curArticolo => curArticolo.id !== id);
 
-  // creo la card che si dorvrà visualizzare
-  <>
-    <div className="container">
-      <section>
-        <h2 className="m-3">Ricette inserite</h2>
-        <div className="row row-cols-2 rowcols-lg-3">
-          {lista.length > 0 ? (
-            lista.map((curItem) => (
-              <div className="col" key={curItem.id}>
-                <div className="card colorcard p-3">
-                  <div className="card-body">
-                    <h4>{curItem.titolo}</h4>
+      setLista(newArray)
+    })
+  }
 
-                    <div className="row row-cols rowcols-lg-3 ">
-                      <img className="immagine col" src={`${apiurl}${curItem.immagine}`} alt="" />
-                      <span className="mx-2 col container_description">{curItem.contenuto} </span>
-                    </div><button onClick={() => cancella(curItem.id)} className="btn btn-danger mt-3">cancella</button>
+
+  return (
+
+    // creo la card che si dorvrà visualizzare
+    <>
+      <div className="container">
+        <section>
+          <h2 className="m-3">Ricette inserite</h2>
+          <div className="row row-cols-2 rowcols-lg-3">
+            {lista.length > 0 ? (
+              lista.map((curItem) => (
+                <div className="col" key={curItem.id}>
+                  <div className="card colorcard p-3">
+                    <div className="card-body">
+                      <h4>{curItem.titolo}</h4>
+
+                      <div className="row row-cols rowcols-lg-3 ">
+                        <img className="immagine col" src={`${apiurl}${curItem.immagine}`} alt="" />
+                        <span className="mx-2 col container_description">{curItem.contenuto} </span>
+                      </div><button onClick={() => cancella(curItem.id)} className="btn btn-danger mt-3">cancella</button>
+                    </div>
+
                   </div>
-                  
                 </div>
-              </div>
-            ))
-          ) : (<p>Nessuna Ricetta presente</p>
+              ))
+            ) : (<p>Nessuna Ricetta presente</p>
 
 
-          )}
-        </div>
-      </section>
+            )}
+          </div>
+        </section>
 
 
-      
-    </div>
-  </>
-);
+
+      </div>
+    </>
+  );
 }
-  
+
 
 
 export default AdminRecipePage;
